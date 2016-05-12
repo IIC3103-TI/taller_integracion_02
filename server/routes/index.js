a3d899e178462dac19edf23f9eef2730ec4621dd
Juan Diego <jddm11@gmail.com>
2016-05-12 20:00:15 -0300

    Added methods to get more information from Instagram

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,18 +3,78 @@ var request = require('request');
 var router  = express.Router();
 
 // *** api routes *** //
-router.post('/instagram/tag/busca', buscaTag);
+router.post('/instagram/tag/busca', search_tag);
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
 
 // *** get recent tags *** //
-function buscaTag(req, res) {
- request({
-  uri:'https://api.instagram.com/v1/tags/'+req.body.tag+'/media/recent?'+
-  'access_token='+req.body.access_token,
-  //55108941.333f3d3.d3e98640f2874c6cb7fe1a114f2e9524',
-  method: "GET"
-}, function(error, response, body) {
-  res.json(body);
-});
+function search_tag(req, res) {
+	if(isEmpty(req.body.tag) || isEmpty(req.body.access_token)){ return res.end(res.writeHead(400, ''));}
+	
+	var tags_option = getTagsCount(req.body.tag,req.body.access_token);
+	request(tags_option,function(error,response, body) {
+		if(error){ res.json({ status:false, result: JSON.parse(error)}); }
+	  	var total_tags = JSON.parse(body);
+	  	if(total_tags.data.media_count > 0)
+	  	{
+		  	var tags_info_option = getTagsInfo(req.body.tag,req.body.access_token);
+		  	request(tags_info_option,function(error_tags,response_tags,body_tags){
+		  		if(error_tags){ return res.json({ status:false, result: JSON.parse(error_tags) }); }
+
+		  		var result = new Object;
+		  		result['metadata'] = new Object;
+		  		result['metadata']['total'] = total_tags.data.media_count;
+		  		result['posts'] = [];
+		  		var info_tags = JSON.parse(body_tags);
+		  		var data = info_tags.data;
+				var len = data.length;
+				for(var i = 0; i < len; i ++ )
+				{
+					var post = new Object;
+					post['tags'] 	 = data[i].tags;
+					post['username'] = data[i].user.username;
+					post['likes']    = data[i].likes.count;
+					post['url']      = data[i].images.standard_resolution.url;
+					post['caption']  = data[i].caption.text;
+ 					result['posts'].push(post);
+				}
+				return res.json(result);
+
+		  	});
+	  	}
+	})
+
+
+	
 }
+
+function getTagsInfo(tag, access_token)
+{
+	return {
+	  uri:'https://api.instagram.com/v1/tags/'+ tag +'/media/recent?'+
+	  'access_token=' + access_token,
+	  method: "GET",
+	  headers: {
+	    'Content-type': 'application/json'
+	  }
+	};
+}
+
+function getTagsCount(tag, access_token)
+{
+	return {
+	  uri:'https://api.instagram.com/v1/tags/'+ tag +'?'+
+	  'access_token='+ access_token,
+	  method: "GET",
+	  headers: {
+	    'Content-type': 'application/json'
+	  }
+	};
+}
+	
+
+
 
 module.exports = router;
--- test/test-server.js
+++ test/test-server.js
@@ -4,23 +4,36 @@ var server = require('../server');
 var access_token = '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402';
 
 describe('Instagram integration', function() {
-  it('should list images from tag search on /instagram/tag/busca POST', function(done) {
-this.timeout(0);
-
- request({
-  uri:'https://api.instagram.com/v1/tags/chile/media/recent?'+
-  'access_token=' + access_token,
-  method: "GET"
-}, function(error, response, body) {
-
-      expect(response).to.exist;
-      expect(response.statusCode).to.equal(200);
-      expect(body).be.json;
-      var json = JSON.parse(response.body);
-      expect(json).to.have.property("data");
-      //expect(json.data).to.have.length.above(0);
-      //expect(data[0]).to.have.property("data");
-     done();
-    });
+  it('should get extra info from tag', function(done) {
+       //this.timeout(0);
+       request({
+        uri:'https://api.instagram.com/v1/tags/chile/media/recent?'+
+        'access_token=' + access_token,
+        method: "GET"
+      }, function(error, response, body) {
+            expect(response).to.exist;
+            expect(response.statusCode).to.equal(200);
+            expect(body).be.json;
+            var json = JSON.parse(response.body);
+            expect(json).to.have.property("data");
+           done();
+          });
   });
+
+   it('should list images from tag search', function(done) {
+     //this.timeout(0);
+     request({
+      uri:'https://api.instagram.com/v1/tags/chile/media/recent?'+
+      'access_token=' + access_token,
+      method: "GET"
+    }, function(error, response, body) {
+
+          expect(response).to.exist;
+          expect(response.statusCode).to.equal(200);
+          expect(body).be.json;
+          var json = JSON.parse(response.body);
+          expect(json).to.have.property("data");
+         done();
+        });
+      });
 });
